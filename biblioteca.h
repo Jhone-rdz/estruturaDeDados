4954b0a741547436b42a52078546854799f08e10
Jhone-rdz <128087416+Jhone-rdz@users.noreply.github.com>
2023-09-26 11:48:10 -0300

    Add files via upload
    ATV de Vetores

---
+++ biblioteca.h
@@ -0,0 +1,9 @@
+#ifndef VETORES_H
+#define VETORES_H
+
+void imprimirVetor(int vetor[], int tamanho);
+int buscarElemento(int vetor[], int tamanho, int elemento);
+void inserirElemento(int vetor[], int *tamanho, int elemento, int posicao);
+int excluirElemento(int vetor[], int *tamanho, int elemento);
+
+#endif
